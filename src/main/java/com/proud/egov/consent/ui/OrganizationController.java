2edfc3f75fd69a78c235008bb1eb34c05c8e8d9a
Andres Kütt <andres.kytt@gmail.com>
2018-10-16 10:30:52 +0300

    Added the ability to get single organisation data

--- src/main/java/com/proud/egov/consent/ui/OrganizationController.java
+++ src/main/java/com/proud/egov/consent/ui/OrganizationController.java
@@ -1,18 +1,25 @@
 package com.proud.egov.consent.ui;
 
+import com.proud.egov.consent.model.Organization;
 import com.proud.egov.consent.repository.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/organizations.json")
-
+@RequestMapping
 public class OrganizationController {
     @Autowired
     private OrganizationRepository organizationRepository;
 
-    @GetMapping
+    @GetMapping("api/organizations")
     public Iterable findAll(){return organizationRepository.findAll();}
+
+    @GetMapping("api/organizations/{id}")
+    public Organization getOrganizationById(@PathVariable(value="id") String orgID){
+        return organizationRepository.findById(orgID)
+                .orElseThrow(() -> new ResourceNotFoundException("Organization", "id", orgID));
+    }
 }
